d8a10e412d51feccc1d7d1ca27ba3c8a870669e7
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2022-02-01 15:29:32 +0530

    Complex Components working without Styling

--- scope/ui/my-button/src/my-button.component.ts
+++ scope/ui/my-button/src/my-button.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'my-button',
   templateUrl: './my-button.component.html',
   styleUrls: ['./my-button.component.scss']
 })
-export class MyButtonComponent {
-  constructor() { }
+export class MyButtonComponent implements OnInit {
 
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+  options: string[] = ['One', 'Two', 'Three', 'Four'];
+
+  filteredOptions: Observable<string[]>;
+
+  constructor(public dialog: MatDialog) {
+    this.filteredOptions = new Observable<string[]>();
+  }
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
 }
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: './dialog-content-example-dialog.html',
+})
+export class DialogContentExampleDialog { }
--- scope/ui/my-button/src/my-button.module.ts
+++ scope/ui/my-button/src/my-button.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import { MyButtonComponent } from './my-button.component';
+import { DialogContentExampleDialog, MyButtonComponent } from './my-button.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    MyButtonComponent
+    MyButtonComponent,
+    DialogContentExampleDialog
   ],
   imports: [
     CommonModule,
@@ -22,7 +25,9 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatDialogModule
   ],
   exports: [
     MyButtonComponent
